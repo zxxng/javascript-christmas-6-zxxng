e4eb04c9ede5ebe09f1b684ca963365daf057a9d
zxxng <jeayeong123@naver.com>
2023-11-11 18:53:06 +0900

    feat: 유효한 주문인지 확인하는 기능 추가

--- src/domain/OrderManager.js
+++ src/domain/OrderManager.js
@@ -4,10 +4,15 @@ class OrderManager {
   #orderList;
 
   constructor(order) {
-    // this.#orderList = parseOrder(order);
+    this.#orderList = this.#parseOrder(order);
+    this.#valitateOrder();
   }
 
-  parseOrder(order) {
+  getOrderList() {
+    return this.#orderList;
+  }
+
+  #parseOrder(order) {
     const orderList = [];
     order.forEach((order) => {
       const orderDetail = {};
@@ -17,6 +22,43 @@ class OrderManager {
     });
 
     return orderList;
+  }
+
+  #valitateOrder() {
+    this.#hasAtLeastOneMenu();
+    this.#isMenuWithinMaxLimit();
+    this.#hasDuplicateMenu();
+  }
+
+  #hasAtLeastOneMenu() {
+    this.#orderList.forEach((order) => {
+      if (order.quantity < 1) {
+        throw new Error(ERROR_MESSEGE.invalidOrder);
+      }
+    });
+  }
+
+  #isMenuWithinMaxLimit() {
+    let totalQuantity = 0;
+    this.#orderList.forEach((order) => {
+      totalQuantity += order.quantity;
+    });
+    if (totalQuantity > 20) {
+      throw new Error(ERROR_MESSEGE.invalidOrder);
+    }
+  }
+
+  #hasDuplicateMenu() {
+    const orderMenus = this.#orderList.map((order) => order.menu);
+    this.#orderList.forEach((order) => {
+      if (this.#duplicateCount(orderMenus, order) >= 2) {
+        throw new Error(ERROR_MESSEGE.invalidOrder);
+      }
+    });
+  }
+
+  #duplicateCount(orderMenus, order) {
+    return orderMenus.filter((menu) => menu === order.menu).length;
   }
 }
 
