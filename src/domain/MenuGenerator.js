d32acaeab5ac164ebf39ffde658535cf3ffbfdd5
zxxng <jeayeong123@naver.com>
2023-11-11 18:21:52 +0900

    feat: 주문 가능한 메뉴인지 확인하는 기능 추가

--- src/domain/MenuGenerator.js
+++ src/domain/MenuGenerator.js
@@ -1,4 +1,5 @@
 import { MENU_LIST } from '../constants/menu';
+import { ERROR_MESSEGE } from '../constants/messege';
 
 class MenuGenerator {
   #menuList;
@@ -26,6 +27,38 @@ class MenuGenerator {
         price: menuItem[1],
       });
     });
+  }
+
+  validateMenu(orderList) {
+    this.#containsInvalidMenuName(orderList);
+    this.#hasOnlyBeverages(orderList);
+  }
+
+  #containsInvalidMenuName(orderList) {
+    const menuNames = this.#menuList.map((menu) => menu.name);
+    orderList.forEach((order) => {
+      if (!menuNames.includes(order.menu)) {
+        throw new Error(ERROR_MESSEGE.invalidOrder);
+      }
+    });
+  }
+
+  #hasOnlyBeverages(orderList) {
+    const isBeverage = true;
+    orderList.forEach((order) => {
+      if (this.#findOrderType(order.menu) !== 'beverage') {
+        isBeverage = false;
+      }
+    });
+
+    if (isBeverage) {
+      throw new Error(ERROR_MESSEGE.invalidOrder);
+    }
+  }
+
+  #findOrderType(orderMenu) {
+    const menu = this.#menuList.find((menu) => menu.name === orderMenu);
+    return menu.type;
   }
 }
 
