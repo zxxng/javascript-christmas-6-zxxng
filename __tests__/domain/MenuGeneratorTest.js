d5bd7ea581030570b77feab07f2d1973c2d2b25e
zxxng <jeayeong123@naver.com>
2023-11-11 18:20:40 +0900

    test: 예외 테스트 코드 수정

--- __tests__/domain/MenuGeneratorTest.js
+++ __tests__/domain/MenuGeneratorTest.js
@@ -5,26 +5,14 @@ describe('MenuGenerator 클래스 테스트', () => {
   const menuGenerator = new MenuGenerator();
   const menuList = menuGenerator.getMenuList();
 
-  console.log(menuList);
   test('menuList가 배열 형태인지 확인한다.', () => {
     expect(Array.isArray(menuList)).toBe(true);
   });
 
-  test('각 메뉴에 type 속성이 있는지 확인한다.', () => {
+  const property = ['type', 'name', 'price'];
+  test.each(property)('각 메뉴에 %s 속성이 있는지 확인한다.', (key) => {
     menuList.forEach((menuItem) => {
-      expect(menuItem).toHaveProperty('type');
-    });
-  });
-
-  test('각 메뉴에 name 속성이 있는지 확인한다.', () => {
-    menuList.forEach((menuItem) => {
-      expect(menuItem).toHaveProperty('name');
-    });
-  });
-
-  test('각 메뉴에 price 속성이 있는지 확인한다.', () => {
-    menuList.forEach((menuItem) => {
-      expect(menuItem).toHaveProperty('price');
+      expect(menuItem).toHaveProperty(key);
     });
   });
 
@@ -33,7 +21,7 @@ describe('MenuGenerator 클래스 테스트', () => {
       { menu: '로제크림파스타', quantity: 1 },
       { menu: '화이트와인', quantity: 1 },
     ];
-    expect(menuGenerator.validateMenu(menu)).toThrow('[ERROR]');
+    expect(() => menuGenerator.validateMenu(menu)).toThrow('[ERROR]');
   });
 
   test('음료만 주문 시 예외가 발생한다.', () => {
@@ -41,6 +29,6 @@ describe('MenuGenerator 클래스 테스트', () => {
       { menu: '제로콜라', quantity: 1 },
       { menu: '레드와인', quantity: 1 },
     ];
-    expect(menuGenerator.validateMenu(menu)).toThrow('[ERROR]');
+    expect(() => menuGenerator.validateMenu(menu)).toThrow('[ERROR]');
   });
 });
