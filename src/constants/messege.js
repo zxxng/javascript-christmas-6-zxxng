f8008ea8fa00b42cf0a38c84188de11e892fe042
zxxng <jeayeong123@naver.com>
2023-11-12 17:41:38 +0900

    feat: 결과를 출력하는 기능 추가

--- src/constants/messege.js
+++ src/constants/messege.js
@@ -7,14 +7,36 @@ export const ERROR_MESSEGE = {
 
 export const WOOTECO_MESSAGE = {
   introduce: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
-  benefitsPreview: (day) => {
-    `12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`;
-  },
+  benefitsPreview: (date) => `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
 };
 
 export const INPUT_MESSAGE = {
-  askReservationDate:
-    '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  askReservationDate: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
   askMenuAndCount:
-    '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+    '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
+};
+
+export const TITLE_MESSAGE = {
+  orderMenu: '<주문 메뉴>',
+  beforeTotalPrice: '<할인 전 총주문 금액>',
+  giftMenu: '<증정 메뉴>',
+  benefitList: '<혜택 내역>',
+  totalDiscount: '<총혜택 금액>',
+  estimatedPayment: '<할인 후 예상 결제 금액>',
+  eventBadge: '<12월 이벤트 배지>',
+};
+
+export const BENEFIT_MESSAGE = [
+  { benefit: 'xmasDiscount', text: '크리스마스 디데이 할인: ' },
+  { benefit: 'weekdayDiscount', text: '평일 할인: ' },
+  { benefit: 'weekendDiscount', text: '주말 할인: ' },
+  { benefit: 'specialdayDiscount', text: '특별 할인: ' },
+  { benefit: 'champagne', text: '증정 이벤트: ' },
+];
+
+export const OUTPUT_FORMAT = {
+  none: '없음',
+  giftMenu: '샴페인 1개',
+  orderMenu: (order) => `${order.menu} ${order.quantity}개`,
+  price: (price) => `${price}원`,
 };
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,91 @@
+import { Console } from '@woowacourse/mission-utils';
+import { TITLE_MESSAGE, BENEFIT_MESSAGE, OUTPUT_FORMAT } from '../constants/messege.js';
+
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printMenu(orderManager, eventManager, menuManager) {
+    const orderList = orderManager.getOrderList();
+    const benefitInfo = eventManager.getBenefitInfo();
+    const totalPrice = orderManager.getTotalPrice(menuManager);
+    const totalDiscount = eventManager.calculateTotalDiscount();
+
+    this.printOrderMenu(orderList);
+    this.printBeforeTotalPrice(totalPrice);
+    this.printGiftMenu(benefitInfo.champagne);
+    this.printBenefitList(benefitInfo);
+    this.printTotalDiscount(totalDiscount);
+    this.printEstimatedPayment(totalPrice, totalDiscount, benefitInfo.champagne);
+    this.printEventBadge(benefitInfo.badge);
   },
-  // ...
+
+  printOrderMenu(orderList) {
+    Console.print(TITLE_MESSAGE.orderMenu);
+    orderList.forEach((order) => {
+      Console.print(OUTPUT_FORMAT.orderMenu(order));
+    });
+    Console.print('');
+  },
+
+  printBeforeTotalPrice(totalPrice) {
+    Console.print(TITLE_MESSAGE.beforeTotalPrice);
+    Console.print(OUTPUT_FORMAT.price(totalPrice));
+    Console.print('');
+  },
+
+  printGiftMenu(champagne) {
+    Console.print(TITLE_MESSAGE.giftMenu);
+    if (champagne) {
+      Console.print(OUTPUT_FORMAT.giftMenu);
+      Console.print('');
+      return;
+    }
+    Console.print(OUTPUT_FORMAT.none);
+    Console.print('');
+  },
+
+  printBenefitList(benefitInfo) {
+    Console.print(TITLE_MESSAGE.benefitList);
+    let isPrinted = false;
+    BENEFIT_MESSAGE.forEach((message) => {
+      if (benefitInfo[message.benefit]) {
+        Console.print(message.text + OUTPUT_FORMAT.price(benefitInfo[message.benefit]));
+        isPrinted = true;
+      }
+    });
+    if (!isPrinted) {
+      Console.print(OUTPUT_FORMAT.none);
+    }
+    Console.print('');
+  },
+
+  printTotalDiscount(totalDiscount) {
+    Console.print(TITLE_MESSAGE.totalDiscount);
+    if (totalDiscount) {
+      Console.print(OUTPUT_FORMAT.price(totalDiscount));
+      Console.print('');
+      return;
+    }
+    Console.print(OUTPUT_FORMAT.none);
+    Console.print('');
+  },
+
+  printEstimatedPayment(totalPrice, totalDiscount, champagne) {
+    Console.print(TITLE_MESSAGE.estimatedPayment);
+    let estimatedPayment = totalPrice - totalDiscount;
+    if (champagne) estimatedPayment += 25000;
+    Console.print(OUTPUT_FORMAT.price(estimatedPayment));
+    Console.print('');
+  },
+
+  printEventBadge(badge) {
+    Console.print(TITLE_MESSAGE.eventBadge);
+    if (badge) {
+      Console.print(badge);
+      Console.print('');
+      return;
+    }
+    Console.print(OUTPUT_FORMAT.none);
+    Console.print('');
+  },
 };
 
 export default OutputView;
