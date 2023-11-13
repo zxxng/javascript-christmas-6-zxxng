0994b624359aa313005d8aeba23f9e9d7d702cd2
zxxng <jeayeong123@naver.com>
2023-11-14 01:22:11 +0900

    refactor: App.js run 함수 분리

--- src/App.js
+++ src/App.js
@@ -7,41 +7,52 @@ import BillManager from './domain/BillManager.js';
 import OutputView from './view/OutputView.js';
 
 class App {
+  #menuManager;
+  #orderManager;
+  #eventManager;
+  #billManager;
+  #date;
+
   constructor() {
-    this.menuManager = new MenuManager();
-    this.orderManager;
-    this.eventManager;
-    this.date;
+    this.#menuManager = new MenuManager();
+    this.#orderManager;
+    this.#eventManager;
+    this.#billManager;
+    this.#date;
   }
 
   async run() {
-    await this.receiveDateInput();
-    await this.receiveOrderInput();
-    this.eventManager.calculateDiscount(this.orderManager, this.menuManager);
-    const billManager = new BillManager(this.orderManager, this.eventManager, this.menuManager);
-    this.eventManager.canGetChampagne(billManager.getTotalPrice());
-    this.eventManager.canGetBadge(billManager.getTotalBenefit());
-    OutputView.printMenu(this.orderManager, this.eventManager, billManager);
+    await this.#receiveDateInput();
+    await this.#receiveOrderInput();
+    this.#applyEventDiscount();
+    OutputView.printMenu(this.#orderManager, this.#eventManager, this.#billManager);
   }
 
-  async receiveDateInput() {
+  async #receiveDateInput() {
     try {
-      this.date = await InputView.readDate();
-      this.eventManager = new EventManager(this.date);
+      this.#date = await InputView.readDate();
+      this.#eventManager = new EventManager(this.#date);
     } catch (err) {
       Console.print(err.message);
-      await this.receiveDateInput();
+      await this.#receiveDateInput();
     }
   }
 
-  async receiveOrderInput() {
+  async #receiveOrderInput() {
     try {
-      const order = await InputView.readOrder(this.date);
-      this.orderManager = new OrderManager(order, this.menuManager);
+      const order = await InputView.readOrder(this.#date);
+      this.#orderManager = new OrderManager(order, this.#menuManager);
     } catch (err) {
       Console.print(err.message);
-      await this.receiveOrderInput();
+      await this.#receiveOrderInput();
     }
+  }
+
+  #applyEventDiscount() {
+    this.#eventManager.calculateDiscount(this.#orderManager, this.#menuManager);
+    this.#billManager = new BillManager(this.#orderManager, this.#eventManager, this.#menuManager);
+    this.#eventManager.canGetChampagne(this.#billManager.getTotalPrice());
+    this.#eventManager.canGetBadge(this.#billManager.getTotalBenefit());
   }
 }
 
