f0018a0824167357553144723c7ce247693ded92
zxxng <jeayeong123@naver.com>
2023-11-15 20:23:40 +0900

    test: 반복되는 부분 함수 분리

--- __tests__/models/EventManagerTest.js
+++ __tests__/models/EventManagerTest.js
@@ -1,19 +1,22 @@
 import EventManager from '../../src/models/EventManager';
 
-const mockOrderManager = {
-  filterItemsByProperty: jest.fn(),
-  getTotalPrice: jest.fn(),
-};
-
-const mockEventManager = {
-  getTotalBenefit: jest.fn(),
-};
-
-const mockOrderMenu = {
-  getOrderMenu: jest.fn(),
-};
+const mockOrderManager = { filterItemsByProperty: jest.fn(), getTotalPrice: jest.fn() };
+const mockEventManager = { getTotalBenefit: jest.fn() };
 
 let eventManager;
+
+const setMockOrderManager = (menuItems, totalPrice, totalBenefit) => {
+  mockOrderManager.filterItemsByProperty.mockReturnValue(menuItems);
+  mockOrderManager.getTotalPrice.mockReturnValue(totalPrice);
+  mockEventManager.getTotalBenefit.mockReturnValue(totalBenefit);
+};
+
+const initEventManagerAndCalculateBenefit = (date) => {
+  eventManager = new EventManager(date);
+  eventManager.calculateDiscountAndBenefit(mockOrderManager);
+
+  return eventManager.getBenefitInfo();
+};
 
 describe('EventManager 클래스 할인 적용 테스트', () => {
   beforeEach(() => {
@@ -22,97 +25,60 @@ describe('EventManager 클래스 할인 적용 테스트', () => {
   });
 
   test('크리스마스 할인이 되는지 확인한다.', () => {
-    eventManager = new EventManager(5);
-    mockOrderManager.filterItemsByProperty.mockReturnValue([]);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
-    const benefitInfo = eventManager.getBenefitInfo();
+    setMockOrderManager([], 0, 0);
+    const benefitInfo = initEventManagerAndCalculateBenefit(5);
     expect(benefitInfo.xmasDiscount).toBe(-1400);
   });
 
   test('특별일자 할인이 되는지 확인한다.', () => {
-    eventManager = new EventManager(17);
-    mockOrderManager.filterItemsByProperty.mockReturnValue([]);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
-    const benefitInfo = eventManager.getBenefitInfo();
+    setMockOrderManager([], 0, 0);
+    const benefitInfo = initEventManagerAndCalculateBenefit(17);
     expect(benefitInfo.specialdayDiscount).toBe(-1000);
   });
 
   test('평일 할인이 되는지 확인한다.', () => {
-    eventManager = new EventManager(11);
-    mockOrderManager.filterItemsByProperty.mockReturnValue([
-      {
-        ...mockOrderMenu,
-        getOrderMenu: jest.fn().mockReturnValue({
-          name: '초코케이크',
-          quantity: 2,
-          price: 15000,
-          category: 'dessert',
-        }),
-      },
-    ]);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
-    const benefitInfo = eventManager.getBenefitInfo();
+    const orderMenu = { name: '초코케이크', quantity: 2, price: 15000, category: 'dessert' };
+    setMockOrderManager([{ getOrderMenu: () => orderMenu }], 0, 0);
+    const benefitInfo = initEventManagerAndCalculateBenefit(11);
     expect(benefitInfo.weekdayDiscount).toBe(-4046);
   });
 
   test('주말 할인이 되는지 확인한다.', () => {
-    eventManager = new EventManager(15);
-    mockOrderManager.filterItemsByProperty.mockReturnValue([
-      {
-        ...mockOrderMenu,
-        getOrderMenu: jest.fn().mockReturnValue({
-          name: '티본스테이크',
-          quantity: 1,
-          price: 55000,
-          category: 'main',
-        }),
-      },
-      {
-        ...mockOrderMenu,
-        getOrderMenu: jest.fn().mockReturnValue({
-          name: '바비큐립',
-          quantity: 1,
-          price: 54000,
-          category: 'main',
-        }),
-      },
-    ]);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
-    const benefitInfo = eventManager.getBenefitInfo();
+    const orderMenu1 = { name: '티본스테이크', quantity: 1, price: 55000, category: 'main' };
+    const orderMenu2 = { name: '바비큐립', quantity: 1, price: 54000, category: 'main' };
+    setMockOrderManager(
+      [{ getOrderMenu: () => orderMenu1 }, { getOrderMenu: () => orderMenu2 }],
+      0,
+      0
+    );
+    const benefitInfo = initEventManagerAndCalculateBenefit(15);
     expect(benefitInfo.weekendDiscount).toBe(-4046);
   });
 
   test('샴페인 증정이 되는지 확인한다.', () => {
-    eventManager = new EventManager(10);
-    mockOrderManager.getTotalPrice.mockReturnValue(120000);
-    mockOrderManager.filterItemsByProperty.mockReturnValue([]);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
-    const benefitInfo = eventManager.getBenefitInfo();
+    setMockOrderManager([], 120000, 0);
+    const benefitInfo = initEventManagerAndCalculateBenefit(10);
     expect(benefitInfo.isChampagne).toBe(true);
   });
 
   test('할인 금액에 따른 배지 증정이 되는지 확인한다.', () => {
-    eventManager = new EventManager(10);
-    mockEventManager.getTotalBenefit.mockReturnValue(-20000);
-    mockOrderManager.filterItemsByProperty.mockReturnValue([]);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
-    const benefitInfo = eventManager.getBenefitInfo();
+    setMockOrderManager([], 120000, -20000);
+    const benefitInfo = initEventManagerAndCalculateBenefit(10);
     expect(benefitInfo.badge).toBe('산타');
   });
 });
 
 describe('EventManager 클래스 할인 금액 계산 테스트', () => {
   test('총 할인 금액 계산 테스트', () => {
-    eventManager = new EventManager(5);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
+    setMockOrderManager([], 0, 0);
+    initEventManagerAndCalculateBenefit(5);
     const totalDiscount = eventManager.getTotalDiscount();
     expect(totalDiscount).toBe(-1400);
   });
 
   test('총 혜택 금액 계산 테스트', () => {
-    eventManager = new EventManager(1);
-    mockOrderManager.getTotalPrice.mockReturnValue(120000);
-    eventManager.calculateDiscountAndBenefit(mockOrderManager);
+    setMockOrderManager([], 120000, 0);
+    initEventManagerAndCalculateBenefit(1);
     const totalBenefit = eventManager.getTotalBenefit();
     expect(totalBenefit).toBe(-26000);
   });
