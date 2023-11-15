150324a89a48386a9d5abae5adbbc8e931454598
zxxng <jeayeong123@naver.com>
2023-11-15 15:12:55 +0900

    fix: 클래스 담당 변경으로 Controller 수정

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,24 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
-import MenuManager from '../models/MenuManager.js';
 import OrderManager from '../models/OrderManager.js';
 import EventManager from '../models/EventManager.js';
-import BillManager from '../models/BillManager.js';
 import { UNIT } from '../constants/options.js';
 
 class Controller {
-  #menuManager;
   #orderManager;
   #eventManager;
-  #billManager;
   #date;
 
   constructor() {
-    this.#menuManager = new MenuManager();
     this.#orderManager;
     this.#eventManager;
-    this.#billManager;
     this.#date;
   }
 
@@ -26,7 +20,7 @@ class Controller {
     await this.#receiveDateInput();
     await this.#receiveOrderInput();
     this.#applyEventDiscount();
-    this.#printOrderDetails();
+    // this.#printOrderDetails();
   }
 
   async #receiveDateInput() {
@@ -39,22 +33,19 @@ class Controller {
   async #receiveOrderInput() {
     await this.#tryCatchHandler(async () => {
       const order = await InputView.readOrder(this.#date);
-      this.#orderManager = new OrderManager(order, this.#menuManager);
+      this.#orderManager = new OrderManager(order);
     });
   }
 
   #applyEventDiscount() {
-    this.#billManager = new BillManager(this.#orderManager, this.#eventManager, this.#menuManager);
-    if (this.#billManager.getTotalPrice(this.#orderManager.getOrderList()) >= UNIT.eventThreshold) {
-      this.#eventManager.calculateDiscount(this.#orderManager, this.#menuManager);
-      this.#eventManager.canGetChampagne(this.#billManager);
-      this.#eventManager.canGetBadge(this.#billManager);
+    if (this.#orderManager.getTotalPrice() >= UNIT.eventThreshold) {
+      this.#eventManager.calculateDiscountAndBenefit(this.#orderManager);
     }
   }
 
-  #printOrderDetails() {
-    OutputView.printResult(this.#orderManager, this.#eventManager, this.#billManager);
-  }
+  // #printOrderDetails() {
+  //   OutputView.printResult(this.#orderManager, this.#eventManager);
+  // }
 
   async #tryCatchHandler(asyncFn) {
     try {
--- src/models/OrderManager.js
+++ src/models/OrderManager.js
@@ -1,4 +1,4 @@
-import Order from './Order';
+import Order from './Order.js';
 import { ERROR_MESSAGE } from '../constants/messages.js';
 import { UNIT } from '../constants/options.js';
 
