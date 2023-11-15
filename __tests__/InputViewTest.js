522fa276c913d912b0843bb5667cb047c07a80bf
zxxng <jeayeong123@naver.com>
2023-11-15 15:13:28 +0900

    test: InputView 테스트 코드 수정

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -25,19 +25,21 @@ describe('InputView 테스트', () => {
     '티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1',
   ];
   const date = 10;
-  test.each(validOrder)('order가 유효할 때 값을 반환하는지 확인한다.', async (order) => {
+  test.each(validOrder)('order 형식이 유효할 때 값을 반환하는지 확인한다.', async (order) => {
     mockQuestions(order);
     expect(await InputView.readOrder(date)).toEqual(order.split(REGEXS.commaAndOptionalSpace));
   });
 
   const invalidOrder = [
-    '타파스-1개,제로콜라-1',
-    '티본스테이크-0',
-    '타파스-1개 제로콜라-1',
+    '타파스-1-1개,제로콜라-1',
+    '크리스마스파스타, 제로콜라-1',
     '티본스테이크1,바비큐립1,초코케이크2,제로콜라1',
   ];
-  test.each(invalidOrder)('order가 유효하지 않을때 예외가 발생하는지 확인한다.', async (order) => {
-    mockQuestions(order);
-    await expect(InputView.readOrder()).rejects.toThrow('[ERROR]');
-  });
+  test.each(invalidOrder)(
+    'order 형식이 유효하지 않을때 예외가 발생하는지 확인한다.',
+    async (order) => {
+      mockQuestions(order);
+      await expect(InputView.readOrder()).rejects.toThrow('[ERROR]');
+    }
+  );
 });
